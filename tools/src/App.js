16dfd489699528cd52961ff6c3a78a4afdeb3537
BuildTools <CynthiaWang7979@hotmail.com>
2020-11-02 18:43:20 +0800

    'debugging...'

--- tools/src/App.js
+++ tools/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import GrouperPage from './pages/grouper_page';
 import ToolsMainPage from './pages/tools_main_page';
 import { MenuComponent } from './components/page_decorations'
